9ff3c8acf80252601ec9a18fa433101e888da5d0
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-09-12 21:59:52 +0530

    Create Doubly_linkedlist.java

---
+++ Doubly_linkedlist.java
@@ -0,0 +1,34 @@
+public class Main
+{
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int x){
+            data = x;
+            next= null;
+            prev = null;
+        }
+    }
+	public static void main(String[] args) {
+		Node head = new Node(10);
+		Node temp1 = new Node(20);
+		Node temp2 = new Node(30);
+		
+		head.next = temp1;
+		temp1.prev = head;
+		temp1.next=temp2;
+		temp2.prev = temp1;
+		
+		printlist(head);
+		
+	}
+	
+	public static void printlist(Node head){
+	     Node curr=head;
+	     while(curr!=null){
+	         System.out.print(curr.data+" ");
+	         curr = curr.next;
+	     }
+	}
+}
